103e4b22309fa21d9ba2ad45645b04b343e42859
pharoon <66510216+pharoon@users.noreply.github.com>
2022-03-29 19:17:33 +0200

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+
 using namespace std;
 
 
@@ -12,16 +13,38 @@ string calc(complex<double> operation)
     return strCin.str();
 }
 
+bool isNumeric(string str) {
+   for (int i = 0; i < str.length(); i++)
+      if (isdigit(str[i]) == false)
+         return false; //when one non numeric value is found, return false
+      return true;
+}
 
 complex<double> complex_number(int num)
 {
     double real, imag;
+    string strReal, strImag;
     cout << "Real [" << num << "]: ";
-    cin >> real;
-    
+    cin >> strReal;
+    while (isNumeric(strReal) == false )
+    {
+        cout << "invalid input please enter a number:";
+        cin.clear();
+        cin.sync();
+        cin >> strReal;
+    }
+    real = stoi(strReal);
 
     cout << "Imaginary [" << num << "]: ";
-    cin >> imag;
+    cin >> strImag;
+    while (isNumeric(strImag) == false )
+    {
+        cout << "invalid input please enter a number:";
+        cin.clear();
+        cin.sync();
+        cin >> strImag;
+    }
+    imag = stoi(strImag);
     return {real, imag};
 }
 
@@ -30,7 +53,7 @@ int main()
 {
     auto x = complex_number(1);
     auto y = complex_number(2);
-    
+
 
     char oper;
     cout << "Enter the operator: ";
